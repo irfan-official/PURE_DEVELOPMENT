076980f79435221eb18a019e3499a137e4bcf088
irfan-official <irfanofficial.work@gmail.com>
2025-02-13 06:45:54 +0600

    complete the todo model

--- Backend_development/Complete_backend_course_by_Hitesh sir/chapter_04/models/todos/sub_todo.models.js
+++ Backend_development/Complete_backend_course_by_Hitesh sir/chapter_04/models/todos/sub_todo.models.js
@@ -1,2 +1,25 @@
+import mongoose from "mongoose";
 
+const sub_todoSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    complete: {
+      type: Boolean,
+      default: false,
+    },
+    under: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Todo",
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamp: true }
+);
 
+export const SubTodo = mongoose.model("SubTodo", sub_todoSchema);
