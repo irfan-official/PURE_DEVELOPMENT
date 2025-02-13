b44d9ec2bde8b4ade451c1dd65a39befbeed8c9c
irfan-official <irfanofficial.work@gmail.com>
2025-02-14 03:35:11 +0600

    connect to the mong atlas

--- Backend_development/Complete_backend_course_by_Hitesh sir/project_video_app/src/index.js
+++ Backend_development/Complete_backend_course_by_Hitesh sir/project_video_app/src/index.js
@@ -1,23 +1,5 @@
 import { config } from "dotenv";
 config();
-import { DB_NAME } from "./constants.js";
-import mongoose from "mongoose";
-import express from "express";
-const app = express();
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-    app.on("error", (error) => {
-      console.log("ERROR: ", error);
-      throw error;
-    });
-    console.log("connected to the database");
-    app.listen(
-      process.env.PORT,
-      console.log(`App is running at "http://localhost:${process.env.PORT}"`)
-    );
-  } catch (error) {
-    console.log("ERROR:", error);
-    throw error;
-  }
-})();
+import connectDB from "./db/index.js";
+
+connectDB();
