6f042d282477583f35602f4c8db5cd280155b077
irfan-official <irfanofficial.work@gmail.com>
2025-02-15 09:56:06 +0600

    complte the alphabet part

---
+++ Frontend_development/FrontendDomination_by_Sheriyan/CSS_Responsive_Project/script.js
@@ -0,0 +1,10 @@
+// let alphabets = document.querySelectorAll("#alphabets span h4");
+
+// alphabets.forEach((h4) => {
+//   h4.onmouseover = () => {
+//     h4.style.border = "1px solid black";
+//   };
+//   h4.onmouseleave = () => {
+//     h4.style.border = "none";
+//   };
+// });
